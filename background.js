8acff55037958a0044ea8e4876d0ed8f21ba5f91
IRespectAkari <145774048+IRespectAkari@users.noreply.github.com>
2025-03-07 19:56:31 +0900

    Add files via upload

---
+++ background.js
@@ -0,0 +1,64 @@
+chrome.runtime.onInstalled.addListener(function (details) {
+  /* コンテキストメニューを作成 */
+  const contextMenu = chrome.contextMenus.create({
+    id: "scrapboxPageCreate",
+    title: "新規ページ作成",
+    contexts: ["all"],
+  });
+});
+
+/* コンテキストメニューがクリックされた時の処理 */
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: script,
+  });
+});
+
+// 簡易型一行版
+// const script = ()=>{var t=window.prompt(`Scrap "${document.title}" to my-knowledge.`,document.title);if (!t) return;var ls=['['+document.title+' '+window.location.href+']'];var q=window.getSelection().toString();if (q.trim()) ls=ls.concat(q.split(/\n/g).map(l=>' > '+l));ls.push('');var b=encodeURIComponent(ls.join('\n'));window.open('https://scrapbox.io/my-knowledge/'+encodeURIComponent(t.trim())+'?body='+b)};
+// const script = ()=>{var t=window.prompt(`Scrap "${document.title}" to my-knowledge.`,document.title);if (!t) return;var ls=['['+document.title+' '+window.location.href+']'];var q=window.getSelection().toString();if (q.trim()) ls=ls.concat(q.split(/\n/g).map(l=>' > '+l));ls.push('');ls.push('#ブックマーク');var b=encodeURIComponent(ls.join('\n'));window.open('https://scrapbox.io/my-knowledge/'+encodeURIComponent(t.trim())+'?body='+b,'新規ページ作成','top=100,left=1000,width=700,height=500')};
+// const script = ()=>{var w=window;var t=document.title;var tt=w.prompt(`Scrap "${t}" to my-knowledge.`,t);if (!tt) return;var ls=['['+tt+' '+w.location.href+']'];var q=w.getSelection().toString();if (q.trim()) ls=ls.concat(q.split(/\n/g).map(l=>' > '+l));ls.push('');ls.push('#ブックマーク');var e=encodeURIComponent;var b=e(ls.join('\n'));w.open('https://scrapbox.io/my-knowledge/'+e(tt.trim())+'?body='+b,'新規ページ作成','top=100,left=1000,width=700,height=500')};
+
+function script() {
+  // 追加するプロジェクト
+  const project = `my-knowledge`;
+  // プロンプトに表示されるテキスト
+  const promptTxt = (Title)=>`Scrap "${Title}" to ${project}.`;
+  // 置換対応マップ
+  const replaceTextList = {
+    "[": "［",
+    "]": "］",
+  };
+  // 置換関数
+  const replaceAllText = (Title, replaceMap) => Object.keys(replaceMap).reduce((pre, cur) => pre.replaceAll(cur, replaceMap[cur]), Title);
+
+  // 処理開始
+  // タイトル処理
+  const originalTitle = document.title;
+  const replacedTitle = replaceAllText(originalTitle, replaceTextList);
+  const title = window.prompt(promptTxt(replacedTitle), replacedTitle);
+  if (!title) return;
+  const encodedTitle = encodeURIComponent(title.trim());
+
+  // 本文作成
+  let lines = [`[${replacedTitle} ${window.location.href}]`];
+  const quote = window.getSelection().toString();
+  if (quote.trim()) {
+    // map後concat
+    // lines = lines.concat(quote.split(/\n/g).map(line=>`	>${line}`));
+
+    // push
+    lines.push(...quote.split(/\n/g).map(line=>`	>${line}`));
+  }
+  lines.push(``);
+  lines.push(`#ブックマーク`);
+  const body = encodeURIComponent(lines.join(`\n`));
+
+  // ページ作成実行
+  window.open(
+    `https://scrapbox.io/${project}/${encodedTitle}?body=${body}`,
+    '新規ページ作成',
+    'top=100,left=1000,width=700,height=500'
+  );
+}
