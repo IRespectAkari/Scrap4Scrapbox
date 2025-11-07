ced5be5355c6e24a2222a7a69423997e8c12f94b
z2251503 <z2251503@std.kiis.ac.jp>
2025-11-07 11:36:52 +0900

    modify to automatically update when project name registration

--- option.js
+++ option.js
@@ -1,15 +1,17 @@
+'use strict';
 
 const defaultProjectName = "my-knowledge";
 
 chrome.storage.local.get("projectName", (data) => {
   const projectName = data["projectName"] || defaultProjectName;
-  $("#nowProjectName").appendChild(document.createTextNode(projectName));
+  $("#nowProjectName").appendChild(create("span", projectName));
   console.log("load projectName: ", projectName);
 });
 
 function setProjectName(e) {
   const newProjectName = $(`input[name="projectName"]`).value;
   chrome.storage.local.set({ projectName: newProjectName });
+  $("#nowProjectName > span").replaceWith(create("span", newProjectName));
   console.log("set projectName: ", newProjectName);
 }
 
