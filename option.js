0790e07229ab9cd1cf7365d7a17aa66abfedddd8
z2251503 <z2251503@std.kiis.ac.jp>
2025-11-07 13:00:41 +0900

    Add processing of tags moreover Integration of initilization process

--- option.js
+++ option.js
@@ -1,18 +1,57 @@
 'use strict';
 
+// デフォルト値
 const defaultProjectName = "my-knowledge";
+const defaultTags = ["途中", "ブックマーク"];
 
-chrome.storage.local.get("projectName", (data) => {
-  const projectName = data["projectName"] || defaultProjectName;
-  $("#nowProjectName").appendChild(create("span", projectName));
-  console.log("load projectName: ", projectName);
-});
+// chrome.storage.local.get("projectName", (data) => {
+//   const projectName = data["projectName"] || defaultProjectName;
+//   $("#nowProjectName").appendChild(create("span", projectName));
+//   console.log("load projectName: ", projectName);
+// });
 
+
+// プロジェクト名をchrome.storageに追加し、nowProjectNameに設定
 function setProjectName(e) {
   const newProjectName = $(`input[name="projectName"]`).value;
   chrome.storage.local.set({ projectName: newProjectName });
   $("#nowProjectName > span").replaceWith(create("span", newProjectName));
   console.log("set projectName: ", newProjectName);
 }
+$("#projectName-registration").addEventListener("click", setProjectName);
 
-$("#registration").addEventListener("click", setProjectName);
+
+// タグが重複してなければchrome.storageに追加し、タグ一覧に追加
+function addTag(e) {
+  const newTag = $(`input[name="tag"]`).value.trim();
+  const tags = $$("#tags > *").map(e=>e.textContent);
+
+  // 既に存在する場合は何もせず終了
+  if (tags.includes(newTag)) return;
+
+  tags.push(newTag);
+  chrome.storage.local.set({ tags: tags });
+  $("#tags").appendChild(create("span", newTag));
+}
+$("#tagAddBtn").addEventListener("click", addTag);
+
+
+// 初期化処理
+async function initializer() {
+  const data = await chrome.storage.local.get(null);
+  const projectName = data["projectName"] || defaultProjectName;
+  const tags = data["tags"] || defaultTags;
+
+  console.log(data);
+  console.log(JSON.stringify(data, null, "  "));
+
+  $("#nowProjectName").appendChild(create("span", projectName));
+  console.log("load projectName: ", projectName);
+
+  const tagsDiv = $("#tags");
+  tags
+    .map(e=>create("span", e))
+    .forEach(e=>tagsDiv.appendChild(e));
+  console.log("load tags: ", tags);
+}
+initializer();
